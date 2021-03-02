d4e96eed5bf4d5b747ab6a6a9e14240c133f36de
MeenuMe <73883057+MeenuMe@users.noreply.github.com>
2021-03-02 15:44:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,15 +13,21 @@ recognition.onresult = function run (event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
+    if(Content == "take my selfie"){
     speak();
+    }
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking your beautiful and wonderful selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function (){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
@@ -31,3 +37,16 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    })
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
